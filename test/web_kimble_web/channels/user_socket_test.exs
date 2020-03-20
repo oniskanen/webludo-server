d8643a43a8672fb39074b5cdfdfcc06580999344
Olli Niskanen <olli.niskanen@gmail.com>
2020-03-20 16:44:15 +0200

    Add test for anonymous connection to channel

---
+++ test/web_kimble_web/channels/user_socket_test.exs
@@ -0,0 +1,9 @@
+defmodule WebKimbleWeb.Channels.UserSocketTest do
+    use WebKimbleWeb.ChannelCase, async: true
+    alias WebKimbleWeb.UserSocket
+
+    test "socket anonymous connection" do
+        assert {:ok, socket} = connect(UserSocket, %{})
+    end
+
+end
