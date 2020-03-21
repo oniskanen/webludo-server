5f8b920a3f1bab1509f72605d6e094eb81c60cdd
Olli Niskanen <olli.niskanen@gmail.com>
2020-03-21 11:10:25 +0200

    Create game as a response to the create_game message in lobby socket

--- lib/web_kimble_web/channels/lobby_channel.ex
+++ lib/web_kimble_web/channels/lobby_channel.ex
@@ -1,13 +1,21 @@
 defmodule WebKimbleWeb.LobbyChannel do
+    alias WebKimble.Networking
+
     use WebKimbleWeb, :channel
 
     def join("lobby", _params, socket) do
         {:ok, socket}
     end
 
-    def handle_in("create_game", params, socket) do
-        broadcast!(socket, "game_created", %{id: 123})
+    defp generate_code do
+        chars = Enum.to_list(?a..?z) ++ Enum.to_list(?0..?9)
+        length = 8
+        Enum.take_random(chars, length)
+    end
 
-        {:reply, :ok, socket}
+    def handle_in("create_game", params, socket) do
+        code = generate_code()
+        game = Networking.create_game(%{code: code})
+        {:reply, {:ok, %{code: code}}, socket}
     end
 end
--- test/web_kimble_web/channels/lobby_channel_test.exs
+++ test/web_kimble_web/channels/lobby_channel_test.exs
@@ -18,6 +18,15 @@ defmodule WebKimbleWeb.Channels.LobbyChannelTest do
         assert_reply ref, :ok, %{}
     end
 
+    test "create_game returns a working game id" do
+        {:ok, socket} = connect(WebKimbleWeb.UserSocket, %{})
+        {:ok, _reply, socket} = subscribe_and_join(socket, "lobby", %{})
+
+        ref = push socket, "create_game", %{}
+
+        assert_reply ref, :ok, %{:code => code}
+    end
+
         
 
 end
