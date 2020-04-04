e1499ca0034f0b447720bf902315ffc4e4b0a31b
Olli Niskanen <olli.niskanen@gmail.com>
2020-04-04 03:34:38 +0300

    Add localhost and netlify frontend to allowed origins

--- config/prod.exs
+++ config/prod.exs
@@ -11,7 +11,8 @@ use Mix.Config
 # before starting your production server.
 config :web_kimble, WebKimbleWeb.Endpoint,
   url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  check_origin: ["//localhost:*", "//webludo.netlify.com"]
 
 # Do not print debug messages in production
 config :logger, level: :info
