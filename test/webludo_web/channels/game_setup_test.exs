323d069e5cb84a02c5eaa95df82d3e47ced76fb6
Olli Niskanen <olli.niskanen@gmail.com>
2020-05-01 22:56:30 +0300

    Add test for checking that start_game sends a game_updated broadcast

--- test/webludo_web/channels/game_setup_test.exs
+++ test/webludo_web/channels/game_setup_test.exs
@@ -66,6 +66,39 @@ defmodule WebLudoWeb.Channels.GameSetupTest do
 
     ref = push(socket, "join_game", %{name: "Player 1"})
     assert_reply ref, :ok, %{token: token1}
+
+    ref = push(socket, "join_game", %{name: "Player 2"})
+    assert_reply ref, :ok, %{token: token2}
+
+    ref = push(socket, "join_game", %{name: "Player 3"})
+    assert_reply ref, :ok, %{token: token3}
+
+    ref = push(socket, "join_game", %{name: "Player 4"})
+    assert_reply ref, :ok, %{token: token4}
+
+    [team1, team2, team3, team4] = game.teams
+
+    push(socket, "join_team", %{token: token1, team_id: team1.id})
+    push(socket, "join_team", %{token: token2, team_id: team2.id})
+    push(socket, "join_team", %{token: token3, team_id: team3.id})
+    push(socket, "join_team", %{token: token4, team_id: team4.id})
+
+    ref = push(socket, "start_game", %{host_token: host_token})
+    assert_reply ref, :ok, %{}
+  end
+
+  test "host sending start_game message after 4 teams are created sends game_updated broadcast" do
+    {:ok, socket} = connect(WebLudoWeb.UserSocket, %{})
+    {:ok, _reply, socket} = subscribe_and_join(socket, "lobby", %{})
+
+    ref = push(socket, "create_game", %{name: "game name"})
+
+    assert_reply ref, :ok, %{code: code, host_token: host_token} = params
+
+    {:ok, %{game: game}, socket} = subscribe_and_join(socket, "games:#{code}", %{})
+
+    ref = push(socket, "join_game", %{name: "Player 1"})
+    assert_reply ref, :ok, %{token: token1}
     assert_broadcast "game_updated", %{}
 
     ref = push(socket, "join_game", %{name: "Player 2"})
@@ -83,11 +116,17 @@ defmodule WebLudoWeb.Channels.GameSetupTest do
     [team1, team2, team3, team4] = game.teams
 
     push(socket, "join_team", %{token: token1, team_id: team1.id})
+    assert_broadcast "game_updated", %{}
     push(socket, "join_team", %{token: token2, team_id: team2.id})
+    assert_broadcast "game_updated", %{}
     push(socket, "join_team", %{token: token3, team_id: team3.id})
+    assert_broadcast "game_updated", %{}
     push(socket, "join_team", %{token: token4, team_id: team4.id})
+    assert_broadcast "game_updated", %{}
 
-    ref = push(socket, "start_game", %{host_token: host_token})
-    assert_reply ref, :ok, %{}
+    push(socket, "start_game", %{host_token: host_token})
+    assert_broadcast "game_updated", %{game: game}
+
+    assert game.has_started
   end
 end
