0c89e180942c80fe83ea96d6b7f18379bb8f854d
Olli Niskanen <olli.niskanen@gmail.com>
2020-05-01 22:53:44 +0300

    Add test for successfully starting a game

--- test/webludo_web/channels/game_setup_test.exs
+++ test/webludo_web/channels/game_setup_test.exs
@@ -53,4 +53,41 @@ defmodule WebLudoWeb.Channels.GameSetupTest do
     ref = push(socket, "start_game", %{host_token: host_token})
     assert_reply ref, :error, %{message: "Cannot start game with less than 4 teams"}
   end
+
+  test "host sending start_game message after 4 teams are created gets ok reply" do
+    {:ok, socket} = connect(WebLudoWeb.UserSocket, %{})
+    {:ok, _reply, socket} = subscribe_and_join(socket, "lobby", %{})
+
+    ref = push(socket, "create_game", %{name: "game name"})
+
+    assert_reply ref, :ok, %{code: code, host_token: host_token} = params
+
+    {:ok, %{game: game}, socket} = subscribe_and_join(socket, "games:#{code}", %{})
+
+    ref = push(socket, "join_game", %{name: "Player 1"})
+    assert_reply ref, :ok, %{token: token1}
+    assert_broadcast "game_updated", %{}
+
+    ref = push(socket, "join_game", %{name: "Player 2"})
+    assert_reply ref, :ok, %{token: token2}
+    assert_broadcast "game_updated", %{}
+
+    ref = push(socket, "join_game", %{name: "Player 3"})
+    assert_reply ref, :ok, %{token: token3}
+    assert_broadcast "game_updated", %{}
+
+    ref = push(socket, "join_game", %{name: "Player 4"})
+    assert_reply ref, :ok, %{token: token4}
+    assert_broadcast "game_updated", %{}
+
+    [team1, team2, team3, team4] = game.teams
+
+    push(socket, "join_team", %{token: token1, team_id: team1.id})
+    push(socket, "join_team", %{token: token2, team_id: team2.id})
+    push(socket, "join_team", %{token: token3, team_id: team3.id})
+    push(socket, "join_team", %{token: token4, team_id: team4.id})
+
+    ref = push(socket, "start_game", %{host_token: host_token})
+    assert_reply ref, :ok, %{}
+  end
 end
